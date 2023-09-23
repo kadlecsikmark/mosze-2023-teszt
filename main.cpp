0caee633fb7b3fdaffc47bfa92c80743647a9b21
kadlecsikmark <55686754+kadlecsikmark@users.noreply.github.com>
2023-09-23 19:26:38 +0200

    első

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+constexpr int N_ELEMENTS = 100;
+
+int main() {
+  
+  int *b = new int[N_ELEMENTS];
+  
+  std::cout << "1-100 ertekek duplazasa";
+  int egytol = 1;
+  for (int i = 0; i < N_ELEMENTS; i++) {
+    b[i] = egytol * 2;
+    egytol++;
+  }
+  
+  for (int i = 0; i < N_ELEMENTS; i++) {
+    std::cout << "Ertek:\t" << b[i] << "\n";
+  }
+  std::cout << "Atlag szamitasa: " << std::endl;
+  int atlag;
+  
+  for (int i = 0; i < N_ELEMENTS; i++) {
+    atlag += b[i];
+  }
+  atlag /= N_ELEMENTS;
+  std::cout << "Atlag: " << atlag << std::endl;
+  return 0;
+}
